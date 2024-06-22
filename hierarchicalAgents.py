4b6f67d75397349939eeb5a00c0f6044b0d65d01
mkolocaj <michaelkolocaj@gmail.com>
2024-06-22 11:55:54 +0200

    Setze Koordinator im HierarchicalAgent vor und passe Initialisierung an; Refaktoriere Funktionen
    - Setze Koordinator im HierarchicalAgent vor und passe die Initialisierungslogik entsprechend an.
    - Refaktoriere mehrere Funktionen zur Verbesserung der Codequalität und Lesbarkeit.

--- hierarchicalAgents.py
+++ hierarchicalAgents.py
@@ -2,13 +2,14 @@ import json
 from multiprocessing import Process
 import time
 from UnitTest import read_sudoku
+import random
 
 
 class HA_Coordinator(Process):
-    def __init__(self, coordinator_queue, connections, domains, csp_numbers, con_dict, *args, **kwargs):
+    def __init__(self, connections, domains, csp_numbers, con_dict, *args, **kwargs):
         super(HA_Coordinator, self).__init__()
         self.name = "coordinator"
-        self.coordinator_queue = coordinator_queue
+        self.coordinator_queue = connections["coordinator"]
         self.connections = connections
         self.domains = domains
         self.con_dict = con_dict
@@ -17,13 +18,27 @@ class HA_Coordinator(Process):
         self.csp_number = 0
         self.number_of_csp = csp_numbers
 
+        self.data_collection = dict()
+        self.collected_data = dict()
+
         self.solving_time = 0
 
         # Dictionary mit den Funktionen zur Behandlung von Nachrichten
         self.message_handlers = {
             "confirm": self.handle_confirm,
-            "start": self.handle_start
+            "unconfirm": self.handle_unconfirm,
+            "start": self.handle_start,
+            "ask_data": self.handle_data_collection
         }
+
+    def prepare_dict(self):
+        preparation_dict = dict()
+        for connection in self.connections.keys():
+            if connection == "coordinator":
+                continue
+            preparation_dict[connection] = False
+        # print(f"Preparation dict: {preparation_dict}")
+        return preparation_dict
 
     def send_message(self, recipient_queue, header, message):
         # Sendet Nachrichten an andere Agenten
@@ -47,14 +62,23 @@ class HA_Coordinator(Process):
 
     def next_csp(self):
         if self.csp_number < self.number_of_csp:
+            self.collected_data = self.prepare_dict()
             self.occupation = read_sudoku(self.csp_number + 1)
-            self.solving_time = time.perf_counter() * 1000
+            con_dict = self.fill_con_dict(self.occupation)
             for connection in self.connections.keys():
-                message = {"domains": self.domains, "occupation": self.occupation,
-                            "csp_number": self.csp_number + 1}
-                self.send_message(self.connections[connection], "startagent", message)
+                if connection == "coordinator":
+                    continue
+                message = {"domains": self.domains, "con_dict": con_dict[connection],
+                           "csp_number": self.csp_number + 1}
+                self.send_message(self.connections[connection], "new_start", message)
             print(f"Starting CSP {self.csp_number + 1}")
+            self.solving_time = time.perf_counter() * 1000
             self.csp_number += 1
+            for key in self.occupation:
+                if self.occupation[key] is None:
+                    self.send_message(self.connections["a1"], "start_solving",
+                              {"occupation": self.occupation, "sender": [], "identifier": []})
+                    return
         else:
             print("All CSPs solved.")
             for connection in self.connections.keys():
@@ -62,32 +86,74 @@ class HA_Coordinator(Process):
                     self.send_message(self.connections[connection], "kill", {"kill": ""})
             self.running = False
 
+    def ask_data(self):
+        for connection in self.connections.keys():
+            if connection == "coordinator":
+                continue
+            self.send_message(self.connections[connection], "ask_data", {"sender": self.name})
+
+    def fill_con_dict(self, occupation):
+        new_con_dict = self.con_dict.copy()
+        for key in new_con_dict:
+            for key2 in new_con_dict[key]:
+                if key2 in occupation and not occupation[key2] is None:
+                    new_con_dict[key][key2] = occupation[key2]
+        return new_con_dict
+
+    def handle_unconfirm(self, message):
+        print("No solution found.")
+        end_time = time.perf_counter() * 1000
+        duration = end_time - self.solving_time
+        print("Zeit für die Lösung:", duration, "ms")
+        self.ask_data()
+
+    def handle_confirm(self, message):
+        print(f"Solution found: {message['occupation']}")
+        end_time = time.perf_counter() * 1000
+        duration = end_time - self.solving_time
+        print("Zeit für die Lösung:", duration, "ms")
+        self.ask_data()
+
+    def handle_data_collection(self, message):
+        for key in message.keys():
+            if key == "sender":
+                continue
+            self.data_collection[key] = self.data_collection.get(key, 0) + message[key]
+        self.collected_data[message["sender"]] = True
+        if all(self.collected_data.values()):
+            print(f"Die Daten des CSP: {self.data_collection}")
+            self.next_csp()
+
+    def handle_start(self, message):
+        print("Starting coordinator")
+        self.number_of_csp = int(message["number_of_csp"])
+        self.next_csp()
+
 
 class HierarchicalAttributAgent(Process):
-    def __init__(self, agent_id, log_queue, name, connections, constraints, all_domains,
-                 occupation, n=4, *args, **kwargs):
+    def __init__(self, agent_id,  name, connections, constraints, *args, **kwargs):
         super(HierarchicalAttributAgent, self).__init__()
         self.agent_id = agent_id  # ID des Agenten
         self.name = name  # Name des Agenten
-        self.log_queue = log_queue  # Queue für Log-Nachrichten
         self.task_queue = connections[self.name]  # Queue für Aufgaben
         self.connections = connections  # Ein Dictionary von Verbindungen zu anderen Agenten
         self.constraints = constraints  # Dict von Constraints zu anderen Agenten
-        self.all_domains = self.unit_testing(occupation, all_domains)  # Liste aller möglicher eigener Domains
-        self.nogood_dict = dict()  # Dictionary mit No-goods Key: Constraint-String Value: Liste von no-good Domains
-        self.agent_view = dict()  # Dictionary mit den Domains, die der Agent betrachtet
-        self.occupation = occupation  # Dictionary mit den belegten Domains des Agenten
+        self.all_domains = None  # Liste aller möglicher eigener Domains
+        self.nogood_dict = None  # Dictionary mit No-goods Key: Constraint-String Value: Liste von no-good Domains
+        self.agent_view = None  # Dictionary mit den Domains, die der Agent betrachtet
+        self.occupation = None  # Dictionary mit den belegten Domains des Agenten
         self.running = True  # Flag, um den Agenten zu stoppen
-        self.n = n  # Anzahl der Agenten
-        self.start_time = None
-        self.end_time = None
+
         self.csp_number = 0
+        self.data_collection_dict = dict()
 
         self.message_handlers = {
-            "check": self.check,
-            "nogood": self.nogood,
-            "kill": self.stop,
-            "startagent": self.startagent
+            "check": self.handle_check,
+            "nogood": self.handle_nogood,
+            "kill": self.handle_stop,
+            "start_solving": self.handle_start_solving,
+            "ask_data": self.handle_data_collection,
+            "new_start": self.handle_new_start
         }
 
     def unit_testing(self, occupation, domains):
@@ -131,12 +197,12 @@ class HierarchicalAttributAgent(Process):
             print("Error: last_sender")
 
     def kill_all(self):
-        # Beendet alle Agenten
-        for connection in self.connections:
-            self.send_message(self.connections[connection], "kill", {"kill": ""})
+        # Beendet Solving, da keine Lösung gefunden wurde
+        self.send_message(self.connections["coordinator"], "unconfirm", {"kill": ""})
 
     def send_message(self, recipient_queue, header, message):
         # Sendet Nachrichten an andere Agenten
+        self.data_collection_dict[header] = self.data_collection_dict.get(header, 0) + 1
         message_data = {"header": header, "message": message}
         recipient_queue.put((self.name, self.csp_number, message_data))
 
@@ -147,12 +213,12 @@ class HierarchicalAttributAgent(Process):
             handler(message)  # Rufe die zugehörige Funktion auf
         else:
             print(f"Received unknown message type: {header} with message: {message}")
-            #self.log(f"Received unknown message type: {header} with message: {message}")
+            # self.log(f"Received unknown message type: {header} with message: {message}")
 
-    def nogood(self, communicationDict):
+    def handle_nogood(self, communicationDict):
         # Aktualisiert das Dictionary der No-goods und probiert die Constraints mit einer neuen Domain
         # zu erfüllen ansonsten wird eine "nogood"-Nachricht an den vorherigen Agenten gesendet
-        #self.log(f"Received nogood from {constraintDict['identifier']}")
+        # self.log(f"Received nogood from {constraintDict['identifier']}")
         # print(f"{self.name} received nogood {communicationDict['occupation'][self.name]}"
         #       f" with Hash: {communicationDict['identifier'][-1]}")
         old_identifier = communicationDict["identifier"][-1]
@@ -180,6 +246,7 @@ class HierarchicalAttributAgent(Process):
         else:
             a = 0
             communicationDict["occupation"][self.name] = self.agent_view[old_identifier].pop()
+            self.data_collection_dict["solution_changed"] = self.data_collection_dict.get("solution_changed", 0) + 1
             for key in self.occupation:
                 if communicationDict["occupation"][key] is None:
                     self.send_message(self.connections[key], "check", communicationDict)
@@ -188,13 +255,10 @@ class HierarchicalAttributAgent(Process):
                     #       f" Domain: {communicationDict['occupation'][self.name]}")
                     return
 
-    def check(self, communicationDict):
+    def handle_check(self, communicationDict):
         # Überprüft, ob es eine Möglichkeit gibt die Constraints zu erfüllen mit
         # den aktuellen Domains-String und fragt die anderen nicht im Domain-String
         # enthaltenen Agenten, ob die Auswahl gültig ist
-        occ_dict = dict()
-        # print(f"{self.name} is checking"
-        #       f" from Sender: {communicationDict['sender'][-1]}")
         if communicationDict["identifier"][-1] == "start":
             hash_identifier = hash("start")
             occ_dict = self.occupation
@@ -203,7 +267,6 @@ class HierarchicalAttributAgent(Process):
 
         if hash_identifier not in self.agent_view and hash_identifier not in self.nogood_dict:
             solve_list = self.solve(communicationDict["occupation"])
-            # print(f"{self.name} is solving with Hash: {hash_identifier} and List: {solve_list}")
             if len(solve_list) == 0:
                 l_sender = self.last_sender(communicationDict["sender"])
                 if l_sender == "start":
@@ -216,33 +279,35 @@ class HierarchicalAttributAgent(Process):
                 self.agent_view[hash_identifier] = set(solve_list)
                 self.nogood_dict[hash_identifier] = set()
         else:
-            # print(f"{self.name} is using old Hash: {hash_identifier}")
             self.agent_view[hash_identifier].update(self.nogood_dict[hash_identifier])
             self.nogood_dict[hash_identifier] = set()
 
-        i = 0
+        i = True
         if len(self.agent_view[hash_identifier]) != 0:
             communicationDict["identifier"].append(hash_identifier)
             communicationDict["occupation"][self.name] = self.agent_view[hash_identifier].pop()
+            self.data_collection_dict["solution_changed"] = self.data_collection_dict.get("solution_changed", 0) + 1
             communicationDict["sender"].append(self.name)
-            for key in occ_dict:
-                if communicationDict["occupation"][key] is None:
-                    self.send_message(self.connections[key], "check", communicationDict)
-                    # print(f"{self.name} is sending check to {key} with Hash: {hash_identifier} and"
-                    #       f" Domain: {communicationDict['occupation'][self.name]} and"
-                    #       f" agent_view: {self.agent_view[hash_identifier]}")
-                    i += 1
-                    return
+            none_keys = [key for key in occ_dict if communicationDict["occupation"][key] is None]
+            if none_keys:
+                random_key = random.choice(none_keys)
+                self.send_message(self.connections[random_key], "check", communicationDict)
+                i = False
         else:
             communicationDict["identifier"].pop()
             communicationDict["sender"].pop()
             communicationDict["occupation"][self.name] = None
             self.send_message(self.connections[self.last_sender(communicationDict["sender"])], "nogood",
                               communicationDict)
-            return
+            i = False
 
-        if i == 0:
+        if i:
             self.send_random(communicationDict)
+
+    def handle_data_collection(self, message):
+        new_message = self.data_collection_dict.copy()
+        new_message["sender"] = self.name
+        self.send_message(self.connections["coordinator"], "ask_data", new_message)
 
     def send_random(self, communicationDict):
         # Sendet eine Nachricht an einen zufälligen nicht gesetzten Agenten
@@ -250,29 +315,26 @@ class HierarchicalAttributAgent(Process):
             if communicationDict["occupation"][agent] is None:
                 self.send_message(self.connections[agent], "check", communicationDict)
                 return
-        print(communicationDict["occupation"])
-        self.kill_all()
+        self.send_message(self.connections["coordinator"], "confirm", communicationDict)
 
-    def stop(self, message):
+    def handle_stop(self, message):
         # Beendet den Agenten
-        #self.log("Received kill message.")
-        #self.log("Stopping agent.")
-        #self.join()
-        self.end_time = time.perf_counter() * 1000
         self.running = False
-        if self.start_time is not None:
-            duration = self.end_time - self.start_time
-            print(f"Zeit für die Lösung: {duration} ms")
-        # print(f"Agent {self.agent_id} ({self.name}) stopped.")
 
-    def startagent(self, communicationDict):
+    def handle_new_start(self, message):
+        self.all_domains = self.unit_testing(message["con_dict"], message["domains"])
+        random.shuffle(self.all_domains)
+        self.nogood_dict = dict()
+        self.agent_view = dict()
+        self.occupation = message["con_dict"]
+        self.csp_number = message["csp_number"]
+        self.data_collection_dict = dict()
+
+    def handle_start_solving(self, communicationDict):
         # Startet den Agenten
-        #self.log("Received start message.")
-        #self.log("Starting agent.")
-        self.start_time = time.perf_counter() * 1000
         communicationDict["sender"].append("start")
         communicationDict["identifier"].append("start")
-        self.check(communicationDict)
+        self.handle_check(communicationDict)
 
     # Löst alle Constraints mit den gegebenen Domains und findet valide Lösungsmenge
     def solve(self, constraintDict):
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from multiprocessing import Process, Queue, Manager
 import time
 from sudoku_problem import Sudoku_Problem
 from UnitTest import read_sudoku
-from hierarchicalAgents import HierarchicalAttributAgent
+from hierarchicalAgents import HA_Coordinator, HierarchicalAttributAgent
 from decentralizedAgents import DA_Coordinator, DecentralizedAttributAgent
 from constraintAgents import CA_Coordinator, ConstraintAgent
-import json
+
 
 def test_constraint():
     start_time = time.perf_counter() * 1000
@@ -52,14 +52,15 @@ def test_constraint():
         for agent in agents:
             agent.join()
 
+
 def test_decentralized():
     start_time = time.perf_counter() * 1000
     multiprocessing.set_start_method('spawn', force=True)
     agents = []
+    n = 9
+    all_domain_list = list(range(1, n + 1))
 
     with Manager() as manager:
-        n = 9
-        all_domain_list = list(range(1, n + 1))
         problem = Sudoku_Problem(n, n_ary=False, conflict=False)
 
         constraint_dict = {}
@@ -81,7 +82,7 @@ def test_decentralized():
                 connections[variable] = manager.Queue()
 
         coordinator = DA_Coordinator(coordinator_queue=connections["coordinator"], connections=connections,
-                                     domains=all_domain_list, csp_numbers=400, con_dict=con_dict)
+                                     domains=all_domain_list, csp_numbers=1, con_dict=con_dict)
         agents.append(coordinator)
 
         i = 1
@@ -96,6 +97,10 @@ def test_decentralized():
         for agent in agents:
             agent.start()
 
+        end_time = time.perf_counter() * 1000
+        duration = end_time - start_time
+        print("Zeit für die Initialisierung:", duration, "ms")
+
         message_data = {"header": "start", "message": ""}
         connections["coordinator"].put(("Start-Main", 0, message_data))
 
@@ -106,12 +111,12 @@ def test_decentralized():
 def test_hierarchy():
     start_time = time.perf_counter() * 1000
     multiprocessing.set_start_method('spawn')
-    log_queue = Queue()
+    agents = []
+    n = 9
+    all_domain_list = list(range(1, n + 1))
+
     with Manager() as manager:
-        n = 9
-        all_domain_list = list(range(1, n + 1))
         problem = Sudoku_Problem(n, n_ary=False, conflict=False)
-        # print(problem.constraints)
         constraint_dict = {}
         con_dict = {}
 
@@ -124,46 +129,42 @@ def test_hierarchy():
                     if connected_var != var:
                         constraint_dict[var][connected_var] = constraint
                         con_dict[var][connected_var] = None
-        print(con_dict)
-
-        occupation_dict = read_sudoku(2)
-
-        for key in con_dict:
-            for key2 in con_dict[key]:
-                if key2 in occupation_dict and not occupation_dict[key2] is None:
-                    con_dict[key][key2] = occupation_dict[key2]
 
         connections = dict()
+        connections["coordinator"] = manager.Queue()
         for cell in problem.cells:
             for variable in cell:
                 connections[variable] = manager.Queue()
 
-        agents = []
+        coordinator = HA_Coordinator(connections=connections, domains=all_domain_list, csp_numbers=1, con_dict=con_dict)
+        agents.append(coordinator)
+
         i = 0
         for cell in problem.cells:
             for variable in cell:
-                agent = HierarchicalAttributAgent(agent_id=i, log_queue=log_queue, name=variable,
-                                                  connections=connections, constraints=constraint_dict[variable],
-                                                  all_domains=all_domain_list, n=n * n, occupation=con_dict[variable])
+                agent = HierarchicalAttributAgent(agent_id=i, name=variable,
+                                                  connections=connections, constraints=constraint_dict[variable])
                 agents.append(agent)
                 i += 1
 
         for agent in agents:
             agent.start()
 
-        agents[1].send_message(agents[0].task_queue, "startagent", {"identifier": [],
-                                                                    "sender": [], "occupation": occupation_dict})
-
         end_time = time.perf_counter() * 1000
         duration = end_time - start_time
         print("Zeit für die Initialisierung:", duration, "ms")
-        log_queue.close()
+
+        message = dict()
+        message["number_of_csp"] = 1
+        message_data = {"header": "start", "message": message}
+        connections["coordinator"].put(("Start-Main", 0, message_data))
+
         for agent in agents:
             agent.join()
 
 
 if __name__ == "__main__":
 
-    # test_hierarchy()
+    test_hierarchy()
     # test_decentralized()
-    test_constraint()
+    # test_constraint()
